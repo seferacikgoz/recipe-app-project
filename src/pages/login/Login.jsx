15badef10dfc7869e3750cfe4eb3f580f89499eb
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-15 03:12:13 +0300

    usenavigate

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import {LoginContainer, FormContainer, StyledImg, Header, StyledInput, StyledForm, StyledButton} from "./Login.style"
+import {useNavigate} from "react-router-dom"
 import meal from "../../assets/meal.svg"
 
 const Login = () => { 
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/")
+  }
+
+
+
   return (
     <LoginContainer>
       <FormContainer>
@@ -10,7 +21,7 @@ const Login = () => {
         <Header>
           {"<CW/>"} Recipe
         </Header>
-        <StyledForm>
+        <StyledForm onSubmit={handleSubmit}>
         <StyledInput placeholder='Enter Username' type="text"/>
         <StyledInput placeholder='Enter Password' type="password"/>
         <StyledButton>          

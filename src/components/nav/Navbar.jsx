9f9b1b3475058cd9d315f4ad815476e5b2b34522
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-17 21:00:29 +0300

    usestate

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,35 +1,29 @@
-import React, { useState } from 'react'
-import {Link} from "react-router-dom"
-import "./Navbar.style"
-import  Nav,{Brand, Menu, MenuLink, Hamburger } from './Navbar.style'
+import React,{useState} from "react";
+import "./Navbar.style";
+import Nav, { Brand ,Menu,MenuLink,Hamburger} from "./Navbar.style";
 import {GiHamburgerMenu} from "react-icons/gi"
 
-
-
-
 const Navbar = () => {
-
-  const [isOpen,setIsOpen] = useState(false)
+  const [isOpen,setIsOpen]=useState(false)
   return (
     <Nav justify="space-between" wrap="wrap">
-    <Brand to ="/">
-      <i>{"<Clarusway/>"}</i>
-    <span>Recipe</span>
-    </Brand>
+      <Brand to="/">
+        <i>{"<Clarusway/>"}</i>
+        <span>Recipe</span>
+      </Brand>
 
-    <Hamburger onClick={() => setIsOpen(!isOpen)}>
-      <GiHamburgerMenu/>
-    </Hamburger>
-  
-    <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
-      <MenuLink to="/">Home</MenuLink>
-      <MenuLink to="about">About</MenuLink>
-      <MenuLink to="register">Register</MenuLink>
-      <MenuLink to="Logout">Logout onClick={() => sessionStorage.clear()} </MenuLink>
-    </Menu>
+      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+        <GiHamburgerMenu/>
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="Logout" onClick={()=>sessionStorage.clear()}>Logout</MenuLink>
+      </Menu>
     </Nav>
-
   );
 };
 
-export default Navbar
+export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,17 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Home = () => {
-  const APP_ID="1d5bc390";
-  const APP_KEY="c3cf9c4080833124490bfba97ca85484";
-  const [query,setQuery] = useState("egg");
-  const[selectedMeal, setSelectedMeal] = useState("breakfast");
+  const APP_ID = "1d5bc390"
+  const APP_KEY = "c3cf9c4080833124490bfba97ca85484"
+  const [query, setQuery] = useState("")
+  const [selectedmeal, setSelectedMeal] = useState("")
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
-  const getData =async() => {
-    const {data}=await axios(url)
-    console.log(data);
+  getData = async () => {
+    const {data} =await axios(url)
   }
-  useEffect(() => {
-    getData()
-  },[])
 
   return (
     <div>

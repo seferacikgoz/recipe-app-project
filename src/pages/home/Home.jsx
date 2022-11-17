f75df4af676fa2a23c786f23e38dc99e37694ca7
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-17 21:15:41 +0300

    useEfect

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,26 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
   const APP_ID = "1d5bc390"
   const APP_KEY = "c3cf9c4080833124490bfba97ca85484"
-  const [query, setQuery] = useState("")
-  const [selectedmeal, setSelectedMeal] = useState("")
+  const [query, setQuery] = useState("egg")
+  const [selectedMeal, setSelectedMeal] = useState("breakfast")
+  const [recipes, setRecipes] = useState("")
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
-  getData = async () => {
-    const {data} =await axios(url)
+  const getData = async () => {
+    const {data} = await axios(url)
+    
+    setRecipes(data.hits)
+    console.log(recipes)
   }
+
+  useEffect(() => {
+    getData()
+  },[])
+
 
   return (
     <div>
